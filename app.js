a2b87bbcad526249d934c1b82ad8851434fb4957
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-11-20 00:20:56 +0100

    added a test file

--- app.js
+++ app.js
@@ -92,50 +92,71 @@ on it cuz its returning html collections
 // })
 
 
+// const list = document.querySelector('#book-list ul');
+
+// // delete books
+// list.addEventListener('click', e => {
+//     if(e.target.className === 'delete'){
+//         const li = e.target.parentElement;
+//         list.removeChild(li);
+//     }
+// })
+
+// // add book-list
+// const addForm = document.forms['add-book'];
+
+// addForm.addEventListener('submit', e => {
+//     e.preventDefault();
+//     const value = addForm.querySelector('input[type="text"]').value;
+    
+//     // create element
+//     const li = document.createElement('li');
+//     const bookName = document.createElement('span');
+//     const deleteBtn = document.createElement('span');
+
+//     // add content
+//     deleteBtn.textContent = 'delete';
+//     bookName.textContent = value;
+
+//     // add classes
+//     bookName.classList.add('name');
+//     deleteBtn.classList.add('delete');
+
+
+//     // append to DOM
+//     li.appendChild(bookName);
+//     li.appendChild(deleteBtn);
+//     list.appendChild(li);
+
+
+
+
+// });
+
+
 const list = document.querySelector('#book-list ul');
 
 // delete books
 list.addEventListener('click', e => {
-    if(e.target.className === 'delete'){
-        const li = e.target.parentElement;
-        list.removeChild(li);
-    }
+    
 })
 
 // add book-list
 const addForm = document.forms['add-book'];
 
 addForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const value = addForm.querySelector('input[type="text"]').value;
     
-    // create element
-    const li = document.createElement('li');
-    const bookName = document.createElement('span');
-    const deleteBtn = document.createElement('span');
-
-    // add content
-    deleteBtn.textContent = 'delete';
-    bookName.textContent = value;
-
-    // add classes
-    bookName.classList.add('name');
-    deleteBtn.classList.add('delete');
-
-
-    // append to DOM
-    li.appendChild(bookName);
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
-
-
-
-
 });
 
-
-
-
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', e => {
+    if(hideBox.checked){
+        list.style.display = "none";
+    } else {
+        list.style.display = "initial";
+    }
+});
 
 
 
